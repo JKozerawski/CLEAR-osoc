8fc0684998a52815fa1b5ce5c61bec2686913341
Jedrzej Kozerawski <jedrzej.kozerawski@muncasterconsulting.com>
2017-10-05 15:29:27 -0700

    Created separate file for shuffling

---
+++ shuffle_in_unison.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def shuffle_in_unison(a, b):
+    assert len(a) == len(b)
+    shuffled_a = np.empty(a.shape, dtype=a.dtype)
+    shuffled_b = np.empty(b.shape, dtype=b.dtype)
+    permutation = np.random.permutation(len(a))
+    for old_index, new_index in enumerate(permutation):
+        shuffled_a[new_index] = a[old_index]
+        shuffled_b[new_index] = b[old_index]
+    return shuffled_a, shuffled_b
